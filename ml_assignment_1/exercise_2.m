ba0374da46d5e61ed7c46a89190113fd8294124b
Vangelis <vtsiat1@gmail.com>
2018-04-05 17:48:33 +0300

    Error counter added

--- ml_assignment_1/exercise_2.m
+++ ml_assignment_1/exercise_2.m
@@ -10,15 +10,18 @@ m2 = 4;
 % Variance of the normal distributions
 s = 1;
 % Total number of points (Give only even numbers)
-N = 100; 
+N = 10000; 
 
 % The vector containing the set of points (1st half from 1st class)
 Y = [randn(1,N/2)+m1 randn(1,N/2)+m2];
+
 % The vector containing the true class labels for the points (if t(i)=1(2), the
 % i-th point comes from class 1(2))
 t = [ones(1, N/2) 2*ones(1, N/2)];
 
-output = []; % Vector containing the class labels of
+% Vector containing the class labels of
+output = [];
+
 for i=1 :N
     %Computation of the pdfs for both classes on the specific data points
     p1 = (1/(sqrt(2*pi)*s))*exp(-(Y(i)-m1)^2/(2*s));
@@ -27,10 +30,21 @@ for i=1 :N
     if(P1*p1 > P2*p2)
         output = [output 1];
     else
-        output = [output 2];
+        output = [output 2];        
     end
 end
 
 % Check for the points that are not classified correctly.
-%if bayes_res(i)=1 then the i-th point is correctly classified
-bayes_res = (t~=output) 
+%if bayes_res(i)=0 then the i-th point is correctly classified
+bayes_res = (t~=output); 
+
+disp(bayes_res);
+
+errors = 0;
+for i=1 :N
+   if(bayes_res(i) == 1)
+       errors = errors + 1;
+   end
+end
+
+disp("Errors " + errors);
