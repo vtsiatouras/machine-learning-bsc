c9a930c0c15f4721234e5c2b1371ef27b159e55f
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2018-04-06 18:01:36 +0300

    Added code for the remaining exercises

---
+++ ml_assignment_1/exercise_4.m
@@ -0,0 +1,81 @@
+% Initialization of the random number generator for normal
+randn('seed',0)
+
+% The parameters of the distributions
+m1 = 1; 
+m2 = 3;
+s = 1;
+
+% Genereation of X1
+%N1 = 15;
+N1 = 500;
+X1 = randn(1,N1)+m1;
+
+% Generation of X2
+%N2 = 15;
+N2 = 500;
+X2 = randn(1,N2)+m2;
+
+% Generation of Y
+% Initialization that guarantees that the same set Y will be produced.
+randn('seed',100) 
+N = 1000;
+Y = [randn(1,N/2)+m1 randn(1,N/2)+m2];
+t = [ones(1,N/2) 2*ones(1,N/2)];
+
+% Maximum likelihood estimates of the means
+m1_ML = sum(X1)/N1;
+m2_ML = sum(X2)/N2;
+
+% Estimation of the a priori probabilities
+P1 = N1/(N1+N2);
+P2 = N2/(N1+N2);
+
+% Bayes rule for the case where the true means are used
+output = [];
+for i=1:N
+    p1 = (1/(sqrt(2*pi)*s))*exp(-(Y(i)-m1)^2/(2*s));
+    p2 = (1/(sqrt(2*pi)*s))*exp(-(Y(i)-m2)^2/(2*s));
+    % Application of the Bayes rule
+    if(P1*p1>P2*p2)
+        output = [output 1];
+    else
+        output = [output 2];
+    end
+end
+% if bayes_res(i)=1 then the i-th point is correctly classified
+bayes_res = (t~=output);
+
+errors_bayes = 0;
+for i=1 :N
+   if(bayes_res(i) == 1)
+       errors_bayes = errors_bayes + 1;
+   end
+end
+
+disp("Errors Bayes: " + errors_bayes);
+
+
+% Bayes rule for the case where the estimated means are used
+output_ML = [];
+for i=1:N
+    p1 = (1/(sqrt(2*pi)*s))*exp(-(Y(i)-m1_ML)^2/(2*s));
+    p2 = (1/(sqrt(2*pi)*s))*exp(-(Y(i)-m2_ML)^2/(2*s));
+    % Application of the Bayes rule
+    if(P1*p1>P2*p2)
+        output_ML = [output_ML 1];
+    else
+        output_ML = [output_ML 2];
+    end
+end
+% if point is correctly classified bayes_res_ML(i)=1 then the i-th
+bayes_res_ML = (t~=output_ML); 
+
+errors_bayes_ML = 0;
+for i=1 :N
+   if(bayes_res_ML(i) == 1)
+       errors_bayes_ML = errors_bayes_ML + 1;
+   end
+end
+
+disp("Errors Bayes ML: " + errors_bayes_ML);
---
+++ ml_assignment_1/exercise_5.m
@@ -0,0 +1,81 @@
+% Initialization of the random number generator for normal
+randn('seed',0) 
+
+%The parameters of the distributions
+m1 = 1; 
+m2 = 3;
+s = 1;
+
+% Genereation of X1
+%N1 = 500;
+N1 = 400;
+X1 = randn(1,N1)+m1;
+
+% Generation of X2
+%N2 = 500;
+N2 = 400;
+X2 = randn(1,N2)+m2;
+
+% Generation of Y
+% Initialization that guarantees that the same set Y will be produced.
+randn('seed',100) 
+N = 1000;
+Y = [randn(1,N/2)+m1 randn(1,N/2)+m2];
+t = [ones(1,N/2) 2*ones(1,N/2)];
+
+% Estimation of the a priori probabilities
+P1 = N1/(N1+N2);
+P2 = N2/(N1+N2);
+
+% Bayes rule for the case where the true means are used
+output = [];
+for i=1:N
+    p1 = (1/(sqrt(2*pi)*s))*exp(-(Y(i)-m1)^2/(2*s));
+    p2 = (1/(sqrt(2*pi)*s))*exp(-(Y(i)-m2)^2/(2*s));
+    % Application of the Bayes rule
+    if(P1*p1>P2*p2)
+        output = [output 1];
+    else
+        output = [output 2];
+    end
+end
+
+% if bayes_res(i)=1 then the i-th correctly classified
+bayes_res=(t~=output);
+
+errors_bayes = 0;
+for i=1 :N
+   if(bayes_res(i) == 1)
+       errors_bayes = errors_bayes + 1;
+   end
+end
+
+disp("Errors Bayes: " + errors_bayes);
+
+
+% Bayes rule for the case where Parzen estimations are used
+h = 0.2;
+output_Parzen = [];
+for i=1:N
+    p1 = sum(abs((X1-Y(i))/h)<=1/2)/(N*h);
+    % This is the 1-D implementation of Parzen
+    p2 = sum(abs((X2-Y(i))/h)<=1/2)/(N*h);
+    % Application of the Bayes rule
+    if(P1*p1>P2*p2)
+        output_Parzen = [output_Parzen 1];
+    else
+        output_Parzen = [output_Parzen 2];
+    end
+end
+
+%if bayes_res_ML(i)=1 then the i-th point is correctly classified
+bayes_res_Parzen = (t~=output_Parzen); 
+
+errors_bayes_Par = 0;
+for i=1 :N
+   if(bayes_res_Parzen(i) == 1)
+       errors_bayes_Par = errors_bayes_Par + 1;
+   end
+end
+
+disp("Errors Bayes Parzen: " + errors_bayes_Par);
