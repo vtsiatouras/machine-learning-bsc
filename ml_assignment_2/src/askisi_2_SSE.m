3f116febf68bc9e1e3eb9369b6473aa7908f7237
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2018-06-02 20:13:36 +0300

    Exercise 2 completed

--- ml_assignment_2/src/askisi_2_SSE.m
+++ ml_assignment_2/src/askisi_2_SSE.m
@@ -23,7 +23,7 @@ z1=[ones(1,fix(N1/2)) 2*ones(1,N1-fix(N1/2))];
 
 % Generate X2 and the required class labels
 %N2=200;
-% N2=10000 % for X3
+%N2=10000 % for X3
 N2=100000 % for X4
 randn('seed',100)
 X2=[mvnrnd(m(:,1),S,fix(N2/2)); mvnrnd(m(:,2),S,N2-fix(N2/2))]';
